0208637b07f5511caf82dbaeccf95f281090f44d
gaurabpokharel <gaurab.pokharel@codehimalaya.com>
2023-07-13 13:16:33 -0400

    code optimize.

--- src/main/java/com/cestar/book/AddBookForm.java
+++ src/main/java/com/cestar/book/AddBookForm.java
@@ -8,7 +8,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class AddBookForm extends JFrame {
